9b7f4cb05ba46f42a0c377ee6c104dd61d7156bb
daisissie <dai.yuxi1112@gmail.com>
2024-07-15 02:32:29 -0400

    Update main.js

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,44 +1,70 @@
-console.log("this works");
-const API_KEY = config.WEATHER_API_KEY;
+console.log('Hello, world!')
+
+// creatre a reference to the button
+let btn = document.querySelector('.search-button')
+let zipcode = document.querySelector('.zipcode')
+let form = document.querySelector('form')
+let img = document.querySelector('img')
+
+let CITY_NAME = document.querySelector('.city_name')
+let CITY_TEMP = document.querySelector('.temperature')
+
+// add an event listener to the button
+btn.addEventListener('click', getCityName)
+
+const apiKey = "0d6fc4d3bb7da9cf19224ea17ac3816c"
 
 
-let input = document.querySelector(".zipcode");
-let btn = document.querySelector(`.search-button`);
-let zipcode = document.querySelector(`.zipcode`)
-
-let form = document.querySelector("form");
-
-let CITY_NAME = document.querySelector(".city-name");
-let CITY_TEMP = document.querySelector(".temperature");
-
-
-function getZipCode(event){
+// event handler
+function getCityName(event) {
     event.preventDefault()
-    let Zip_Code = zipcode.value;
-    getWeatherData(Zip_Code)
+    let city_name = zipcode.value;
+    console.log(city_name)
+    getWeatherData(city_name)
 }
 
-// write a function to get weather data
-const getWeatherData = (zip) => {
-    // store your open weather API Key
-    const API_KEY = "[YOUR API KEY HERE]";
-    // store the API endpoint and API key
-    const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
+function getWeatherData(city) {
+const Url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    // fetch request goes in here :-)
+    fetch(Url)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        const weatherData = data
 
-    fetch(API_ENDPOINT)
-        .then(response => response.json())
-        .then(data => {
-        // store the requested data in a variable
-        let local_weather_data = data;
-        // manipulate the city name content
-        CITY_NAME.textContent = getWeatherData.name;
-        // process the temperature data before manipulating the content
-        let weather_in_celsius = Math.round(
-            local_weather_data.main.temp - 273
-        );
-        // manipulate the temperature content
-        CITY_TEMP.textContent = weather_in_celsius + " C"
+        let weather_in_celcius = Math.round(weatherData.main.temp - 273.15)
+
+        CITY_NAME.textContent = weatherData.name
+        CITY_TEMP.textContent = weather_in_celcius
+
+        const temp = weatherData.main.temp
+        const feelsLike = weatherData.main.feels_like
+        const humidity = weatherData.main.humidity
+        const windSpeed = weatherData.wind.speed
+        const weatherDescription = weatherData.weather[0].description
+        const weatherIcon = weatherData.weather[0].icon
+        const weatherIconUrl = `http://openweathermap.org/img/w/${weatherIcon}.png`
+        const weatherLocation = weatherData.name
+        const weatherCountry = weatherData.sys.country
+
+        console.log(weatherIcon)
+        console.log(weatherIconUrl)
+
+        img.setAttribute('src', weatherIconUrl)
+
+        // console.log(temp, feelsLike, humidity, windSpeed, weatherDescription, weatherIcon, weatherLocation, weatherCountry)
+        // document.getElementById('temp').innerText = temp
+        // document.getElementById('feels-like').innerText = feelsLike
+        // document.getElementById('humidity').innerText = humidity
+        // document.getElementById('wind-speed').innerText = windSpeed
+        // document.getElementById('weather-description').innerText = weatherDescription
+        // document.getElementById('weather-icon').src = weatherIconUrl
+        // document.getElementById('weather-location').innerText = weatherLocation
+        // document.getElementById('weather-country').innerText = weatherCountry
+
+    form.reset()
+    zipcode.focus()
     });
 }
 
-btn.addEventListener('click', getZipCode);
+// getWeatherData()
