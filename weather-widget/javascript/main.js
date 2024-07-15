6f361e8eb989671107e23cd448735a0e299fc2d2
daisissie <dai.yuxi1112@gmail.com>
2024-07-15 02:31:00 -0400

    1

--- weather-widget/javascript/main.js
+++ weather-widget/javascript/main.js
@@ -1,37 +1,44 @@
 console.log("this works");
 const API_KEY = config.WEATHER_API_KEY;
 
+
 let input = document.querySelector(".zipcode");
-let btn = document.querySelector(".search-button");
+let btn = document.querySelector(`.search-button`);
+let zipcode = document.querySelector(`.zipcode`)
+
 let form = document.querySelector("form");
 
 let CITY_NAME = document.querySelector(".city-name");
 let CITY_TEMP = document.querySelector(".temperature");
-let zip = input.value
 
-function getWeatherData() {
-    const getWeatherData = (zip) => {e}
-    const API_KEY = config.WEATHER_API_KEY;
+
+function getZipCode(event){
+    event.preventDefault()
+    let Zip_Code = zipcode.value;
+    getWeatherData(Zip_Code)
+}
+
+// write a function to get weather data
+const getWeatherData = (zip) => {
+    // store your open weather API Key
+    const API_KEY = "[YOUR API KEY HERE]";
+    // store the API endpoint and API key
     const API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&APPID=${API_KEY}`;
-    
-    fetch(API_ENDPOINT)    
-        .then((response) => response.json())
-        .then((data) => {
-            let local_weather_data = data;
-            CITY_NAME.textContent = local_weather_data.name;
-            let weather_in_celsius = Math.round(
-                local_weather_data.main.temp - 273
-            );
-            CITY_TEMP.textContent = weather_in_celsius + " C"
+
+    fetch(API_ENDPOINT)
+        .then(response => response.json())
+        .then(data => {
+        // store the requested data in a variable
+        let local_weather_data = data;
+        // manipulate the city name content
+        CITY_NAME.textContent = getWeatherData.name;
+        // process the temperature data before manipulating the content
+        let weather_in_celsius = Math.round(
+            local_weather_data.main.temp - 273
+        );
+        // manipulate the temperature content
+        CITY_TEMP.textContent = weather_in_celsius + " C"
     });
-    form.reset();
-    input.focus();
 }
 
-const getZipcode = e => {
-    e.preventDefault();
-    let zip = input.value;
-    getWeatherData(zip);
-}
-
-btn.addEventListener('click', getZipcode);
+btn.addEventListener('click', getZipCode);
